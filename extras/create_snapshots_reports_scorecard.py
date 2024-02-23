3fcb6e2e9f48c176dfcebec54b00ee42ee377cde
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-02-23 11:48:40 -0500

    Bump up the number of report and snapshot threads
    We arrived at the values in the code after a lot of trial and error.
    See the comments in #111 for more details.
    
    Co-authored-by: dav3r <david.redmin@gwe.cisa.dhs.gov>
    Co-authored-by: Nick <50747025+mcdonnnj@users.noreply.github.com>

--- extras/create_snapshots_reports_scorecard.py
+++ extras/create_snapshots_reports_scorecard.py
@@ -40,8 +40,8 @@ current_time = util.utcnow()
 
 LOGGING_LEVEL = logging.INFO
 LOG_FILE = "snapshots_reports_scorecard_automation.log"
-REPORT_THREADS = 18
-SNAPSHOT_THREADS = 32
+REPORT_THREADS = 40
+SNAPSHOT_THREADS = 40
 
 NCATS_DHUB_URL = "dhub.ncats.cyber.dhs.gov:5001"
 NCATS_WEB_URL = "web.data.ncats.cyber.dhs.gov"
